ad302d1bd1c19fa4bdc42e038121f1e675b1e650
서성진 <tjtjdwls0614@gmail.com>
2025-03-05 16:05:25 +0900

    react_counterApp UI 구현

--- React/CounterAPP/src/App.jsx
+++ React/CounterAPP/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Viewer from './component/Viewer'
+import Controller from './component/Controller'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
   )
 }
 
---
+++ React/CounterAPP/src/component/Viewer.jsx
@@ -0,0 +1,10 @@
+export default function Viewer() {
+  return (
+    <>
+      <div>
+        <div>현재 카운터 :</div>
+        <h1>0</h1>
+      </div>
+    </>
+  )
+}
