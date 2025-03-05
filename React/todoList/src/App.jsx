4e5dbf33d7a06bbb18b8af3a6b7cf5902cac1491
서성진 <tjtjdwls0614@gmail.com>
2025-03-05 19:52:26 +0900

    react todoList ui 구현

--- React/todoList/src/App.jsx
+++ React/todoList/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Header from './components/Header'
+import List from './components/List'
+import Editor from './components/Editor'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
   )
 }
 
---
+++ React/todoList/src/components/List.jsx
@@ -0,0 +1,16 @@
+import './List.css'
+import TodoItem from './TodoItem'
+
+export default function List()  {
+  return (
+    <div className='List'>
+      <h4>Todo List🌱</h4>
+      <input placeholder="검색어를 입력하세요" />
+      <div className='todos_wrapper'>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  )
+}
