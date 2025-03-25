1ed4068d60dff64bfa4a04ba064b05ba5870ee44
서성진 <tjtjdwls0614@gmail.com>
2025-03-25 12:16:23 +0900

    TS:복습: 객체

--- TypeScript/studyTS/section2/src/chapter3.ts
+++ TypeScript/studyTS/section2/src/chapter3.ts
@@ -7,6 +7,9 @@ let user: {
   name: "서성진"
 };
 
+user.id; // 객체 리터럴 타입으로 점 표기법으로 접근 가능
+
+
 let dog: {
   name: string;
   color: string;
@@ -16,11 +19,14 @@ let dog: {
 };
 
 // 구조적 타입 시스템
+// 구조를 기준으로 타입 정의
+// 프로퍼티 베이스드 타입 시스템
+
 
 user = {
   name: "홍길동",
 }
-// ? 선택적프로퍼티, 옵셔널프로퍼티 -> 있어도 되고, 없어도 상관 없음
+// id? 선택적프로퍼티, 옵셔널프로퍼티 -> 있어도 되고, 없어도 상관 없음
 
 
 let config: {
@@ -28,6 +34,6 @@ let config: {
 } = {
   apikey: "My API key",
 }
-
 // config.apikey = "hacked"
+// readonly를 통해 읽기 전용으로 만들수 있음(변경 불가)
 // 읽기 전용 속성이므로 'apikey'에 할당할 수 없음
