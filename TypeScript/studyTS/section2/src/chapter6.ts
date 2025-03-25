33d4e08e7cdede07ed245cdc512ee12bd43e63b8
서성진 <tjtjdwls0614@gmail.com>
2025-03-25 14:07:28 +0900

    TS:복습: any와 unknown 타입

--- TypeScript/studyTS/section2/src/chapter6.ts
+++ TypeScript/studyTS/section2/src/chapter6.ts
@@ -1,5 +1,6 @@
 // any -> 모든, 누구나
 // 특정 변수의 타입을 우리가 확실히 모를때
+
 let anyVar: any = 10;
 anyVar = "hello";
 
@@ -10,6 +11,8 @@ anyVar = () => {};
 anyVar.toUpperCase();
 anyVar.toFixed();
 
+let num: number = 10;
+num = anyVar;
 // 런타임에 에러 발생 가능성
 // 가급적 사용하지 않는것이 옳음
 
@@ -17,6 +20,14 @@ anyVar.toFixed();
 
 // unknown
 let unknownVar:  unknown;
+unknownVar ="";
+unknownVar = 1;
+unknownVar = () => {};
+
 // 다른 변수에 할당 불가
 // 정제해서 사용해야함 -> 타입 정제 typeof
+// 타입 좁히기
+if (typeof unknownVar === "number") {
+  num = unknownVar;
+}
 
