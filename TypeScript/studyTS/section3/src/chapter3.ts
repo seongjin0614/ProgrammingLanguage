df6a6f7261015b06adbd0abed0d1bb2f8dd5f320
서성진 <tjtjdwls0614@gmail.com>
2025-03-27 09:23:33 +0900

    TS:복습: 객체 타입의 호환성

--- TypeScript/studyTS/section3/src/chapter3.ts
+++ TypeScript/studyTS/section3/src/chapter3.ts
@@ -1,6 +1,8 @@
 // 기본 타입간의 호환성
+let num1: number = 10;
+let num2: 10 = 10;
 
-
+num1 = num2;
 
 
 // 객체 타입간의 호환성
@@ -30,3 +32,23 @@ let dog: Dog = {
 
 animal = dog;
 // dog = animal; // 'breed' 속성이 'animal' 형식에 없지만 'Dog' 형식에서 필수입니다.
+// 프로퍼티를 가지고 슈퍼, 서브타입을 판단
+
+
+type Book = {
+  name: string,
+  price: number,
+};
+
+type ProgrammingBook = {
+  name: string,
+  price: number,
+  skill: string,
+};
+
+let book : Book;
+let programmingBook: ProgrammingBook = {
+  name: "한입",
+  price: 33000,
+  skill: "react"
+}
