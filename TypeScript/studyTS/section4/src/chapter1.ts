31400748a0ed1c7d4fd5cbc22f8a9d27093c17a7
서성진 <tjtjdwls0614@gmail.com>
2025-03-27 17:03:30 +0900

    TS:복습: 함수 타입 표현식과 호출 시그니처

--- TypeScript/studyTS/section4/src/chapter1.ts
+++ TypeScript/studyTS/section4/src/chapter1.ts
@@ -8,10 +8,12 @@ const add2 = (a:number, b:number): number => a+b;
 // 함수 타입 표현식을 사용하면 중복된 코드를 줄일 수 있다.
 
 const sub: Operation = (a, b) => a - b;
-
+const multuply: Operation = (a, b) => a*b
 
 // 호출 시그니처 (콜 시그니차)
 type Operation2 = {
   (a: number, b:number): number;
   name: string
 }
+
+const add3: Operation2 = (a,b) => a+b;
