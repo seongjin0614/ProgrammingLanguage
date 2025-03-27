265d4dc377193b6af0ecb10108e5565ebe17b219
서성진 <tjtjdwls0614@gmail.com>
2025-03-27 09:14:16 +0900

    TS:복습: 타입은 집합이다

--- TypeScript/studyTS/section3/src/chapter2.ts
+++ TypeScript/studyTS/section3/src/chapter2.ts
@@ -1,4 +1,5 @@
 // unknown 타입
+// 전체 집합이라고 볼수 있음
 
 function unknownExam() {
   let a: unknown = 1;
@@ -9,15 +10,38 @@ function unknownExam() {
 
   let unknownVar: unknown;
 
+  // let num: number = unknownVar; 불가능, 다운캐스팅 불가
+  // let num: string = unknownVar;
 }
 
 
 // never 타입
+// 공집합으로 이해할수 있음
 function neverExam() {
   function neverFunc(): never {
     while (true) {}
   }
 }
 
-// any 타입
 
+// void 타입
+function voidExam() {
+  function voidFunc(): void {
+    console.log("hi");
+  }
+}
+
+
+// any 타입
+// 모든 타입의 슈퍼타입으로 위치하기도 하며, 모든 타입의 서브타입으로 위치하기도 한다(never 제외)
+function anyExam() {
+  let unknownVar: unknown;
+  let anyVar: any;
+  let undefinedVar: undefined;
+
+  anyVar = unknownVar;
+
+  undefinedVar = anyVar;
+
+  // neverVar = 
+}
