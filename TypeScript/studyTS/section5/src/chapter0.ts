9efcf873c0bdf5de5572bf399176c297a5617226
서성진 <tjtjdwls0614@gmail.com>
2025-03-28 15:37:16 +0900

    TS:복습: 인터페이스

--- TypeScript/studyTS/section5/src/chapter0.ts
+++ TypeScript/studyTS/section5/src/chapter0.ts
@@ -1,12 +1,22 @@
 // 인터페이스
+// 객체 타입의 구조 정의에 특화된 문법
+// 상속, 합친 등의 특수한 기능을 제공
 
 interface Person {
-  name: string;
-  age: number;  
+  readonly name: string;
+  age?: number;
+  sayHi(): void;
+  sayHi(a:number, b: number): void;
 }
 
 const person: Person = {
   name: "이정환",
-  age: 12
-}
+  age: 12,
+  sayHi: function () {
+    console.log("hi")
+  }
+};
 
+
+person.sayHi();
+person.sayHi(1,2);
