e39b4c142a7fa61213d72f3babf9bd1a12f30e3e
서성진 <tjtjdwls0614@gmail.com>
2025-03-28 15:28:46 +0900

    TS:복습: t사용자 정의 타입가드

--- TypeScript/studyTS/section4/src/chapter4.ts
+++ TypeScript/studyTS/section4/src/chapter4.ts
@@ -15,3 +15,16 @@ type Animal = Dog | Cat;
 function isDog(animal: Animal): animal is Dog {
   return (animal as Dog).isBark !== undefined;
 }
+
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isScratch ! == undefined;
+}
+
+function warning(animal: Animal) {
+  if(isDog(animal)) {
+    // 강아지
+    animal;
+  } else if ("isScratch" in animal) {
+    // 고양이
+  }
+}
